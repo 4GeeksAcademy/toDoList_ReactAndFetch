fd127d9e7a2f0f4f16edeaa0f3e84c53534ab3b5
Luis Guerrero <158310573+LuisGuerrero7@users.noreply.github.com>
2024-08-27 01:33:33 +0000

    falta algo de estilos

--- src/js/component/lista.jsx
+++ src/js/component/lista.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Task from "./task";
 
-
-
 const ToDoList = (props) => {
 
     const [newTask,setTask] = useState('');
@@ -15,32 +13,43 @@ const ToDoList = (props) => {
         setTaskList(data.todos)
     }
 
-    const createNewTask = async () => {
-        const response = await fetch("https://playground.4geeks.com/todo/todos/luisGuerrero",{
-            method: "POST",
-            body: {
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(newTask)
-            }
-        })
-        const data = await response.json();
-        setTask(data.todos) 
+    const createNewTask = async (task) => {
+        try {
+            const response = await fetch("https://playground.4geeks.com/todo/todos/luisGuerrero",{
+                method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(task)
+            })
+            if(!response.ok){
+                throw new Error("There has been an error creating this task, check again")
+            } 
+            loadTasksFromAPI();
+            setTask('')
+        }
+        catch (error) {
+            console.log("There has been an error creating this task")
+        }
     }
-
-    // const deleteTask = async () => {
-    //     const response = await fetch("https://playground.4geeks.com/todo/todos/" + ,{
-    //         method: "DELETE",
-    //     })
-    // }
-
 
     useEffect(()=>{
         loadTasksFromAPI();
-        createNewTask();
+    },[])
+
+    const handleSubmit = () => {
+        if (newTask.trim() !== '') {
+            const task = {
+                label: newTask,
+                done: false
+            };
+            createNewTask(task);
+        }
     }
-    ,[])
+
+    const handleInputChange = (e) => {
+        setTask(e.target.value)
+    }
 
     return(
         <div className="container pt-4 m-3">
@@ -49,22 +58,20 @@ const ToDoList = (props) => {
                 type="text" 
                 value={newTask} 
                 placeholder="What needs to be done?" 
-                onChange={(event) => setTask(event.target.value)}
+
+                onChange={handleInputChange}
+
                 onKeyUp={(event) => {
-                    if(event.key == 'Enter'){
-                        createNewTask([newTask,...taskList]);
-                        setTask('')
+                    if(event.key === 'Enter'){
+                        handleSubmit();
                     }
-                }   
-                }
+                }}
             />
 
-            {(taskList.length == 0) && <div>No tasks, add task</div>}
+            {(taskList.length === 0) && <div>No tasks, add task</div>}
             
-            {taskList.map( (tarea,index) => <Task task={tarea} key={index} onRemove = { () => {
-                setTaskList(taskList.filter(
-                    (_tarea,indiceABorrar) => index != indiceABorrar
-                ))
+            {taskList.map( (tarea,index) => <Task task={tarea} taskId={tarea.id} key={index} onRemove = { () => {
+                loadTasksFromAPI();
             }}
             />)}
             <p className="pt-2 m-3">{taskList.length} items left</p>
--- src/js/component/task.jsx
+++ src/js/component/task.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { useState } from "react";
+import ToDoList from "./lista";
+
 
 const Task = (props) => {
 
     const [isHover, setIsHover] = useState(false);
 
+    const deleteTask = async (id) => {
+        try{
+            const response = await fetch("https://playground.4geeks.com/todo/todos/" + id,{
+                method: "DELETE",
+            })
+            if(!response.ok){
+                throw new Error("There has been an error deleting this task, check again")
+            };
+        }
+        catch (error) {
+            console.log("There has been an error deleting this task")
+        }        
+    }
+
+    const handleDelete = (id) => {
+        console.log(id)
+        deleteTask(id)
+    }
+
     return(
         <div className="d-flex justify-content-center w-100">
-            <div className="d-flex justify-content-between border p" style={{width: "50%", alignSelf: "center", }}
+            <div className="d-flex justify-content-between border p-1 m-1" style={{width: "50%", alignSelf: "center",borderColor:"black" }}
                 onMouseEnter={
                     () => {
                     setIsHover(true);
@@ -23,6 +44,8 @@ const Task = (props) => {
                 {(isHover) && <button
                     type="button" className="btn-close danger"
                     onClick={() => {
+                        
+                        handleDelete(props.taskId);
                         props.onRemove()
                     }}
                 />}
